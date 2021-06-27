b661e749cff4da1fb343820bbd4f851a53d48d1d
rodrigods93 <rodrigodds96@gmail.com>
2021-06-27 14:03:56 -0300

    NLW parte 4 inclusão do banco de dados

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+//na linha de comando para fechar o comando usar CTRL+C
+const route = require('./route')
+const path = require('path')
+
+const server = express()//inicia o express
+
+
+server.set('view engine', 'ejs')
+
+server.use(express.static("public"))
+
+server.set('views',path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
+
+server.use(route)
+
+server.listen(3000, ()=> console.log("Rodando"))//inicia o servidor rodando em uma porta 
+
